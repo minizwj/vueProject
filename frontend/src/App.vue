26078eb544863dffa388d7472441318f65f6f335
yangxue12 <2325449412@qq.com>
2023-12-12 20:19:04 +0800

    1.3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <div id="app">
-    <el-button type="primary">这是一个按钮</el-button>
+    <Index></Index>
   </div>
 </template>
 
 <script>
+import Index from "./components/Index";
 
 
 export default {
   name: 'App',
   components: {
-
+    Index
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
